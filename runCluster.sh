84fed0835e3ad9e632b4eee06acd2ce54539a2e0
Ben Rotter <BenJRotter@gmail.com>
2016-12-05 09:21:11 -1000

    fixed the hostname stuff

--- runCluster.sh
+++ runCluster.sh
@@ -6,11 +6,13 @@ numEntries=540800
 entriesPerCluster=$((numEntries/4))
 entriesPerCore=$((numEntries/256))
 clusterNum=0
-if   [ `hostname` == "anitai.phys.hawaii.edu" ];   then clusterNum=0;
-elif [ `hostname` == "anitaii.phys.hawaii.edu" ];  then clusterNum=1;
-elif [ `hostname` == "anitaiii.phys.hawaii.edu" ]; then clusterNum=2;
-elif [ `hostname` == "anitaiv.phys.hawaii.edu" ];  then clusterNum=3;
+if   [ `hostname` == "anitaI.phys.hawaii.edu" ];   then clusterNum=0;
+elif [ `hostname` == "anitaII.phys.hawaii.edu" ];  then clusterNum=1;
+elif [ `hostname` == "anitaIII.phys.hawaii.edu" ]; then clusterNum=2;
+elif [ `hostname` == "anitaIV.phys.hawaii.edu" ];  then clusterNum=3;
 else echo "You aren't on a server I recognize!"; fi
+
+echo "You are on server "${clusterNum}
 
 for core in `seq 0 64`; do
     startEv=$((entriesPerCluster*clusterNum + core*entriesPerCore))
